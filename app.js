f7ccc2671ed6ec80e6b8266b6d72bfed900743cc
phkyww <paingheink2003@gmail.com>
2025-02-11 12:23:54 +0630

    add show to users function

--- app.js
+++ app.js
@@ -5,17 +5,48 @@ let infoText = document.querySelector(".info-text");
 let list = document.querySelector("ul");
 list.style.display = "none";
 
+//show to Users
+function showToUsers(data) {
+  console.log(data);
+  list.style.display = "block";
+
+  //word and type
+  document.querySelector(".word p").innerHTML = data[0].word;
+  let type = data[0].phonetic;
+  if (type) {
+    document.querySelector(
+      ".word span"
+    ).innerText = `${data[0].meanings[0].partOfSpeech} ${type}`;
+  } else {
+    document.querySelector(
+      ".word span"
+    ).innerText = `${data[0].meanings[0].partOfSpeech}`;
+  }
+
+  //meaning
+  document.querySelector(".meaning span").innerText =
+    data[0].meanings[0].definitions[0].definition;
+
+  //example
+  let eg = data[0].meanings[1].definitions[0].example;
+  if (eg) {
+    document.querySelector(".example span").innerText = eg;
+  } else {
+    document.querySelector(".example").style.display = "none";
+  }
+}
+
 //show words
 function showWords(result, word) {
   //checking that searched word is exist or not
   if (result.title) {
     infoText.style.color = "red";
     infoText.innerHTML = `<b>${word}</b> doesn't exist!!`;
+    list.style.display = "none";
   } else {
     userInput.value = "";
-    infoText.style.color = "green";
-    infoText.innerHTML = `searched <b>${word}</b>`;
-    console.log(result);
+    infoText.innerText = "";
+    showToUsers(result);
   }
 }
 
@@ -23,6 +54,8 @@ function showWords(result, word) {
 function searchWords(word) {
   infoText.style.color = "#000";
   infoText.innerText = `searching ${word}`;
+  list.style.display = "none";
+
   fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     .then((response) => response.json())
     .then((result) => showWords(result, word));
